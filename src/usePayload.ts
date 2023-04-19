7116f34b24ee3a0348e6d3e7e170567334c94e42
wsvaio <wsvaio@qq.com>
2023-04-19 10:06:50 +0800

    fix(usePayload): 重写$clear逻辑

--- src/usePayload.ts
+++ src/usePayload.ts
@@ -1,5 +1,5 @@
 import type { DeepPartial, Middleware } from "@wsvaio/utils";
-import { compose, merge } from "@wsvaio/utils";
+import { compose, merge, pick } from "@wsvaio/utils";
 import type { UnwrapNestedRefs } from "vue";
 import { inject, onBeforeUnmount, provide, reactive } from "vue";
 
@@ -13,7 +13,7 @@ export type Payload<Initial extends object = {}> = {
   $action: (...names: string[]) => (...options: DeepPartial<Payload<Initial>>[]) => Promise<void>;
   $use: (...names: string[]) => (...middlewares: Middleware<Payload<Initial>>[]) => void;
   $unuse: (...names: string[]) => (...middlewares: Middleware<Payload<Initial>>[]) => void;
-  $clear: () => void;
+  $clear: (...keys: (keyof Initial)[]) => void;
 } & Initial;
 
 const wrapper = <T extends object>(type: "use" | "unuse") => (...maps: Map<Middleware<Payload>, Set<string>>[]) => (...names: string[]) => (...middlewares: Middleware<Payload<T>>[]) => {
@@ -64,9 +64,9 @@ export const usePayload = <Initial extends object>(initial = {} as Initial, opti
         await c(payload);
       },
 
-      $clear: () => {
-        merge(payload, merge({}, initial, { deep: Infinity }), { del: true });
-        merge(payload, { $loading: false });
+      $clear: (...keys: (keyof Initial)[]) => {
+        keys.length <= 0 && (keys = Object.keys(initial).filter(key => !key.startsWith("$")) as any);
+        merge(payload, merge({}, pick(initial, keys), { deep: Infinity }), { del: true });
       },
     } as Payload<Initial>);
   }
